469e98c9cd722936d2455452e08bbdd39d4b597d
KovalYevhenii <kovalyevhenii@gmail.com>
2023-12-11 15:00:26 +0100

    Models updated

--- GServer/Models/Enemies/IEnemy.cs
+++ GServer/Models/Enemies/IEnemy.cs
@@ -5,5 +5,7 @@ namespace GServer.Models.Enemies
     public interface IEnemy
     {
         public List<Enemy> ThrowDice(int numberOfDice);
+        public DiceSide GetRandomDiceSide();
+
     }
 }
