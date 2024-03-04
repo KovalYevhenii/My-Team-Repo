b34c0298ed01aded4493eea8cc872929722440df
KovalYevhenii <kovalyevhenii@gmail.com>
2024-03-04 21:47:59 +0100

    sessionManager

---
+++ GameClient/TelegramBotClient/Session/SessionManager.cs
@@ -0,0 +1,26 @@
+﻿namespace TelegramBotClient.Session;
+internal class SessionManager
+{
+    private readonly Dictionary<long, UserSession> _sessions = new();
+
+    public UserSession CreateSession(long userId, long chatId)
+    {
+        var session = new UserSession
+        {
+            ChatId = chatId,
+            UserId = userId,
+            SessionId = Guid.NewGuid().ToString(),
+        };
+        _sessions[userId] = session;
+        return session;
+    }
+    public UserSession GetSession(long userId)
+    {
+        _sessions.TryGetValue(userId, out var session);
+        return session;
+    }
+    public void DeleteSession(long userId)
+    {
+        _sessions.Remove(userId);
+    }
+}
---
+++ GameClient/TelegramBotClient/Session/UserSession.cs
@@ -0,0 +1,8 @@
+﻿namespace TelegramBotClient.Session;
+
+public class UserSession
+{
+    public string SessionId { get; set; }
+    public long UserId { get; set; }
+    public long ChatId { get; set; }
+}
