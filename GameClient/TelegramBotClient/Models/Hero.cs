a2e36efae8e6a6fa5f42145843b92546a0f4eb15
Bender <perfiliev.ds@gmail.com>
2024-03-06 19:15:01 +0300

    Add ApiClient

---
+++ GameClient/TelegramBotClient/Models/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotClient.Models
+{
+    public class Hero
+    {
+        public string hash { get; set; }
+        public HeroType type { get; set; }
+    }
+
+    public enum HeroType
+    {
+        ENCHANTRESS,
+        SOLDEROFFORTUNE,
+        KNIGHT,
+        BARD,
+        MAGE,
+        NECROMANCER,
+        CRUSADER,
+        GOBLIN
+    }
+}
---
+++ GameClient/TelegramBotClient/Services/ApiClient.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using TelegramBotClient.Models;
+
+namespace TelegramBotClient.Services
+{
+    public class ApiClient : IDisposable
+    {
+        private HttpClient client;
+
+        private string apiUrl = "localhost";
+        private string errMsg = "";
+        private string sessionId = "";
+
+        public ApiClient()
+        {
+            client = new HttpClient();
+            client.Timeout = Timeout.InfiniteTimeSpan;
+        }
+
+        public string? GetSessionID(ChatId chatId)
+        {
+            errMsg = "";
+            try
+            {
+                HttpResponseMessage _sessionId = client.PostAsJsonAsync<long?>("get/user/sessionId", chatId.Identifier).Result;
+                if (_sessionId.StatusCode != HttpStatusCode.OK)
+                {
+                    errMsg = $"Ошибка получения сесии: {_sessionId.Content.ReadAsStringAsync().Result}";
+                    return null;
+                }
+                return JsonConvert.DeserializeObject<string>(_sessionId.Content.ReadAsStringAsync().Result);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+        public Dictionary<string, HeroType>? GetHero(long sessionId)
+        {
+            errMsg = "";
+            try
+            {
+                HttpResponseMessage _hero = client.PostAsJsonAsync<long?>("get/hero", sessionId).Result;
+                if (_hero.StatusCode != HttpStatusCode.OK)
+                {
+                    errMsg = $"Ошибка создания героя: {_hero.Content.ReadAsStringAsync().Result}";
+                    return null;
+                }
+                return JsonConvert.DeserializeObject<Dictionary<string, HeroType>>(_hero.Content.ReadAsStringAsync().Result);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+}
